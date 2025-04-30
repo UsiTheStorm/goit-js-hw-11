308a83314d5a868f5b2b9bd2da06f08e68618df0
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-30 14:07:08 +0100

    Add pixabay API request function

---
+++ src/js/pixbay-api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY = '49754838-8d67938118f09bcabafca9026';
+const BASE_URL = 'https://pixabay.com/api/';
+
+// Get images by query using api
+// Link example: pixabay.com/api/?key=49754838-8d67938118f09bcabafca9026&q=dog&image_type=photo&orientation=horizontal
+export default function getImagesByQuery(query) {
+    return axios
+        .get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+            },
+        })
+        .then(({ data }) => data)
+        .catch((error) => {
+            console.error('❌ Failed to fetch images:', error.message);
+            throw error;
+        });
+}
