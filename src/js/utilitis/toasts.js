18876da1f5da963dd5cefedfdb10a5437d42bea1
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-30 12:02:59 +0100

    Instal izitoast

--- src/js/utilitis/toasts.js
+++ src/js/utilitis/toasts.js
@@ -2,48 +2,48 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 // Function to show a success toast
-export const showSuccessToast = message => {
-  iziToast.success({
-    title: '✅',
-    // backgroundColor: '#a6a2e4',
-    message,
-    position: 'bottomRight',
-    transitionIn: 'bounceInRight',
-    transitionOut: 'fadeOut',
-    progressBar: true,
-    theme: 'light',
-    animateInside: true,
-    timeout: 5000,
-  });
+export const showSuccessToast = (message) => {
+    iziToast.success({
+        title: '✅',
+        // backgroundColor: '#a6a2e4',
+        message,
+        position: 'bottomRight',
+        transitionIn: 'bounceInRight',
+        transitionOut: 'fadeOut',
+        progressBar: true,
+        theme: 'light',
+        animateInside: true,
+        timeout: 5000,
+    });
 };
 
 // Function to show an error toast
-export const showErrorToast = message => {
-  iziToast.error({
-    title: '❌',
-    message,
-    position: 'bottomRight',
-    transitionIn: 'bounceInRight',
-    transitionOut: 'fadeOut',
-    progressBar: true,
-    theme: 'light',
-    animateInside: true,
-    timeout: 5000,
-  });
+export const showErrorToast = (message) => {
+    iziToast.error({
+        title: '❌',
+        message,
+        position: 'bottomRight',
+        transitionIn: 'bounceInRight',
+        transitionOut: 'fadeOut',
+        progressBar: true,
+        theme: 'light',
+        animateInside: true,
+        timeout: 5000,
+    });
 };
 
 // Function to show a warning toast
-export const showWarningToast = message => {
-  iziToast.warning({
-    title: '',
-    message,
-    backgroundColor: '#a6a2e4',
-    position: 'bottomRight',
-    transitionIn: 'bounceInRight',
-    transitionOut: 'fadeOut',
-    progressBar: true,
-    theme: 'light',
-    animateInside: true,
-    timeout: 5000,
-  });
+export const showWarningToast = (message) => {
+    iziToast.warning({
+        title: '',
+        message,
+        backgroundColor: '#a6a2e4',
+        position: 'bottomRight',
+        transitionIn: 'bounceInRight',
+        transitionOut: 'fadeOut',
+        progressBar: true,
+        theme: 'light',
+        animateInside: true,
+        timeout: 5000,
+    });
 };
